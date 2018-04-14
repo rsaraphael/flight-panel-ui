db559af412c4e028a0dcebec3b6278afa312a6c0
Raphael Alves <rsaraphael@gmail.com>
2018-04-14 14:54:05 -0300

    Minor fixes

--- src/app/flight/flight-info.controler.js
+++ src/app/flight/flight-info.controler.js
@@ -6,7 +6,6 @@ function FlightInfoController($stateParams, FlightService) {
     const vm = this;
     FlightService.getFlightInfo($stateParams.flightNumber)
         .then(setData);
-    console.log(vm.flightInfo)
 
     function setData(response) {
         vm.flightInfo = response.data;
---
+++ src/app/flight/flight-number.directive.js
@@ -0,0 +1,22 @@
+angular
+    .module("app")
+    .directive("flightNumberPanel", flightNumberDirective);
+
+
+function flightNumberDirective() {
+    const directive = {
+        templateUrl: 'app/flight/flight-number.html',
+        scope: {
+            flight: '='
+        },
+        controller: flightNumberController,
+        controllerAs: 'vm',
+        bindToController: true
+    };
+
+    return directive;
+}
+
+function flightNumberController() {
+    const vm = this;
+}
