e34f65b053891daec7518f231e6d0dd2d27e1bbf
Raphael Alves <rsaraphael@gmail.com>
2018-04-15 13:56:16 -0300

    Add advanced search

--- src/app/flight/flight.controller.js
+++ src/app/flight/flight.controller.js
@@ -1,9 +1,24 @@
 angular
-  .module("app")
-  .controller("FlightController", FlightController);
+  .module('app')
+  .controller('FlightController', FlightController);
 
 function FlightController(FlightService, $rootScope) {
   const vm = this;
   $rootScope.loadPromise = FlightService.getFlights();
-  $rootScope.loadPromise.then(response => vm.flightNumbers = response.data);
+  $rootScope.loadPromise.then(setFlightData);
+  vm.enableAdvancedSearchFilter = show => {
+    vm.showFilters = show;
+  };
+
+  vm.advancedSearch = () => {
+    FlightService.getFlightByParams(vm.origin, vm.destination, vm.pilotName, vm.aircraftModel)
+      .then(setFlightData);
+  };
+
+  function setFlightData(response) {
+    vm.flightNumbers = response.data;
+  }
+
+  vm.enableAdvancedSearchFilter(false);
 }
+
--- src/app/services/flight.service.js
+++ src/app/services/flight.service.js
@@ -2,12 +2,23 @@ angular.module('app').service('FlightService', FlightService);
 
 /** @ngInject */
 function FlightService($http) {
-    return {
-     getFlights() {
-       return $http.get('http://localhost:8080/flight');
+  return {
+    getFlights() {
+      return $http.get('http://localhost:8080/flight');
     },
     getFlightInfo(id) {
       return $http.get('http://localhost:8080/flight/' + id);
-   }
+    },
+    getFlightByParams(origin, destination, pilotName, aircraftModel) {
+      const config = {
+        params: {
+          origin,
+          destination,
+          pilotName,
+          aircraftModel
+        }
+      };
+      return $http.get('http://localhost:8080/flight/search', config);
+    }
   };
 }
